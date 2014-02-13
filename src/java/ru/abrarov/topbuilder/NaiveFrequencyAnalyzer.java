262aa823dc485423485cb55395022b5acc0e4f4e
Marat Abrarov <abrarov@gmail.com>
2014-02-13 19:48:29 +0400

    Initial commit.

---
+++ src/java/ru/abrarov/topbuilder/NaiveFrequencyAnalyzer.java
@@ -0,0 +1,126 @@
+package ru.abrarov.topbuilder;
+
+import java.util.*;
+
+/**
+ * Naive implementation of the FrequencyAnalyzer.
+ * Uses {@link java.util.HashMap java.util.HashMap} to build distribution of the analyzed values so the number of unique
+ * values (among the analyzed ones) and their hashes distribution are the most significant factors for memory
+ * consumption (the first of this factors) and performance.
+ * The most narrow place is usage of {@link java.util.Collections#sort java.util.Collections.sort}
+ * so the complexity of {@link #buildTopFrequentList} is o(n * log(n)).
+ *
+ * @see FrequencyAnalyzer
+ */
+public class NaiveFrequencyAnalyzer implements FrequencyAnalyzer {
+
+    @Override
+    public List<Item> buildTopFrequentList(Iterator<String> values, int size) {
+        assert size >= 0 : "Size of the list must be >= 0";
+        final List<DistributionItem> valueDistribution =
+                new ArrayList<DistributionItem>(buildDistributionMap(values).values());
+        final List<DistributionItem> topFrequentDistributionItems =
+                findTopFrequentDistributionItems(valueDistribution, size);
+        return buildResult(topFrequentDistributionItems);
+    }
+
+    /**
+     * Builds distribution map of the given values.
+     *
+     * @param values Values to be analyzed. Nulls are permitted.
+     * @return Map of values distribution.
+     */
+    private Map<String, DistributionItem> buildDistributionMap(Iterator<String> values) {
+        final Map<String, DistributionItem> distribution = new HashMap<String, DistributionItem>();
+        while (values.hasNext()) {
+            final String value = values.next();
+            DistributionItem distributionItem = distribution.get(value);
+            if (distributionItem == null) {
+                distributionItem = new DistributionItem(value);
+                distribution.put(value, distributionItem);
+            }
+            distributionItem.incFrequency();
+        }
+        return distribution;
+    }
+
+    /**
+     * Finds items of the given distribution having max frequency.
+     *
+     * @param distribution Distribution to search in.
+     * @param count        Maximum number of returned items.
+     * @return List of items of the given distribution having max frequency.
+     */
+    private List<DistributionItem> findTopFrequentDistributionItems(List<DistributionItem> distribution, int count) {
+        //todo: This implementation is rather naive and may be much better.
+        //todo: JCF, Guava, Apache Commons or another ready solution needs to be searched for.
+        Collections.sort(distribution, new Comparator<DistributionItem>() {
+            @Override
+            public int compare(DistributionItem left, DistributionItem right) {
+                final int leftFrequency = left.frequency();
+                final int rightFrequency = right.frequency();
+                if (leftFrequency > rightFrequency) {
+                    return -1;
+                }
+                if (leftFrequency < rightFrequency) {
+                    return 1;
+                }
+                // If frequencies are equal than compare values to make results of algorithm predictable
+                final String leftValue = left.value();
+                final String rightValue = right.value();
+                if (leftValue == null) {
+                    if (rightValue == null) {
+                        return 0;
+                    }
+                    return 1;
+                }
+                return leftValue.compareTo(rightValue);
+            }
+        });
+        final int uniqueValueCount = distribution.size();
+        final int topFrequentListSize = Math.min(uniqueValueCount, count);
+        return distribution.subList(0, topFrequentListSize);
+    }
+
+    /**
+     * Builds list of Item from the given collection of DistributionItem
+     *
+     * @param distributionItems The source List which contents will be copied to the built list of Item.
+     * @return Built list of Item.
+     */
+    private List<Item> buildResult(Collection<DistributionItem> distributionItems) {
+        final List<Item> result = new ArrayList<Item>(distributionItems.size());
+        for (DistributionItem distributionItem : distributionItems) {
+            result.add(distributionItem);
+        }
+        return result;
+    }
+
+    private static class DistributionItem implements Item {
+
+        private final String value;
+        private int frequency;
+
+        public DistributionItem(String value) {
+            this.value = value;
+            this.frequency = 0;
+        }
+
+        @Override
+        public String value() {
+            return value;
+        }
+
+        @Override
+        public int frequency() {
+            return frequency;
+        }
+
+        /**
+         * Increments stored frequency.
+         */
+        public void incFrequency() {
+            ++frequency;
+        }
+    }
+}
---
+++ src/java/ru/abrarov/topbuilder/RandomUtils.java
@@ -0,0 +1,69 @@
+package ru.abrarov.topbuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Utilities for generation of random values.
+ * May be replaced with another ready solution. Implemented here to make app depends only on JRE 1.5.
+ */
+public class RandomUtils {
+
+    private RandomUtils() {
+    }
+
+    /**
+     * Generates random integer that lays in [min; max).
+     *
+     * @param random Random generator to be used.
+     * @param min    Minimum value of generated integer. Must be <= max.
+     * @param max    Maximum value of generated integer. Must be >= min.
+     * @return Generated integer that lays in [min; max).
+     */
+    public static int randomInt(Random random, int min, int max) {
+        assert min <= max : "min must be <= max";
+        return random.nextInt(max - min) + min;
+    }
+
+    /**
+     * Generates random string.
+     *
+     * @param alphabet Alphabet to be used in string generation. Must be not empty.
+     * @param length   Length of the generated string. Must be >= 0.
+     * @param random   Random generator to be used.
+     * @return String of desired length containing random characters of desired alphabet.
+     */
+    public static String randomString(char[] alphabet, int length, Random random) {
+        assert length >= 0 : "length must be >= 0";
+        assert alphabet.length > 0 : "alphabet must be not empty";
+        final StringBuilder valueBuilder = new StringBuilder(length);
+        for (int j = 0; j < length; ++j) {
+            final int alphabetPos = randomInt(random, 0, alphabet.length);
+            valueBuilder.append(alphabet[alphabetPos]);
+        }
+        return valueBuilder.toString();
+    }
+
+    /**
+     * Generates list containing random number of any value from the given collection.
+     *
+     * @param values   Collection of values used to fill generated list.
+     * @param listSize Size of the list to be generated.
+     * @param random   Random generator to be used.
+     * @param <T>      Value type.
+     * @return Generated list containing random number of any value from the given collection.
+     */
+    public static <T> List<T> randomFilledList(List<T> values, int listSize, Random random) {
+        assert !(values.isEmpty() && listSize > 0) : "listSize > 0 is not permitted for non empty values";
+        assert !values.isEmpty() && listSize < 0 : "listSize <= 0 is permitted for empty values only";
+        final int valuesCount = values.size();
+        final List<T> result = new ArrayList<T>(listSize);
+        for (int i = 0; i < listSize; i++) {
+            final int randomIndex = randomInt(random, 0, valuesCount);
+            result.add(values.get(randomIndex));
+        }
+        return result;
+    }
+
+}

a353f6b3f73e2be9d6ab5afdfd6dd9f00651fbce
Marat Abrarov <abrarov@gmail.com>
2014-03-23 22:25:37 +0400

    Switched to Maven.

---
+++ top-builder/src/main/java/ru/abrarov/javatests/topbuilder/FrequencyAnalyzer.java
@@ -0,0 +1,45 @@
+package ru.abrarov.javatests.topbuilder;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Algorithm that builds the list of the most frequent values. The contract is
+ * defined by task definition.
+ * 
+ * @see NaiveFrequencyAnalyzer
+ */
+public interface FrequencyAnalyzer {
+
+	/**
+	 * Top list item.
+	 */
+	public static interface Item {
+
+		/**
+		 * Gets the value of the top list item.
+		 * 
+		 * @return Value of the top list item. Nullable.
+		 */
+		String value();
+
+		/**
+		 * Gets the frequency of the top list item value.
+		 * 
+		 * @return Frequency of the top list item value.
+		 */
+		int frequency();
+	}
+
+	/**
+	 * Builds the list of the most frequent values.
+	 * 
+	 * @param values
+	 *            Source values to be analyzed. Null iterator values are
+	 *            permitted.
+	 * @param size
+	 *            The maximum size of the list to be built. Must be >= 0.
+	 * @return List of the most frequent values with their frequencies.
+	 */
+	public List<Item> buildTopFrequentList(Iterator<String> values, int size);
+}
---
+++ top-builder/src/main/java/ru/abrarov/javatests/topbuilder/example/RandomSourceDataProvider.java
@@ -0,0 +1,66 @@
+package ru.abrarov.javatests.topbuilder.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Builder of list of random strings.
+ */
+public class RandomSourceDataProvider {
+
+	private static final char[] RANDOM_ALPHABET = "0123546789ABCDEFGHIJKLMNOPQRSTUWXWZabcdefghijklmnopqrstuwxwz"
+			.toCharArray();
+	private final Random random;
+
+	public RandomSourceDataProvider() {
+		this.random = new Random();
+	}
+
+	/**
+	 * Builds list of random strings.
+	 * 
+	 * @param minValueLength
+	 *            Minimum length of string to be generated. Must be >= 0 and <=
+	 *            maxValueLength.
+	 * @param maxValueLength
+	 *            Maximum length of string to be generated. Must be >= 0 and >=
+	 *            minValueLength.
+	 * @param uniqueValueCount
+	 *            Number of unique strings to be generated. Must be >= 0.
+	 * @param totalValueCount
+	 *            Total number of strings to be generated. Equals to the size
+	 *            (defines the size) of the built list. Must be >= 0.
+	 * @return List of the desired size (totalValueCount) containing random
+	 *         strings of the desired length limits.
+	 */
+	public List<String> buildRandomValues(int minValueLength,
+			int maxValueLength, int uniqueValueCount, int totalValueCount) {
+		assert minValueLength >= 0 : "minValueLength must be >= 0";
+		assert maxValueLength >= 0 : "maxValueLength must be >= 0";
+		assert uniqueValueCount >= 0 : "uniqueValueCount must be >= 0";
+		assert totalValueCount >= 0 : "totalValueCount must be >= 0";
+		assert minValueLength <= maxValueLength : "minValueLength must be <= maxValueLength";
+
+		final List<String> uniqueValues = buildUniqueValues(minValueLength,
+				maxValueLength, uniqueValueCount);
+		return RandomUtils.randomFilledList(uniqueValues, totalValueCount,
+				random);
+	}
+
+	private List<String> buildUniqueValues(int minValueLength,
+			int maxValueLength, int uniqueValueCount) {
+		final List<String> values = new ArrayList<String>(uniqueValueCount);
+		for (int i = 0; i < uniqueValueCount; ++i) {
+			values.add(buildRandomString(minValueLength, maxValueLength));
+		}
+		return values;
+	}
+
+	private String buildRandomString(int minLength, int maxLength) {
+		final int stringLength = RandomUtils.randomInt(random, minLength,
+				maxLength + 1);
+		return RandomUtils.randomString(RANDOM_ALPHABET, stringLength, random);
+	}
+
+}

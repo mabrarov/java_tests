b8d8509df0de6356bf78b31b11c2334ef98a6072
Marat Abrarov <abrarov@gmail.com>
2014-05-06 00:18:53 +0400

    Minor refactoring was done.

--- top-builder/src/main/java/ru/abrarov/javatests/topbuilder/NaiveFrequencyAnalyzer.java
+++ top-builder/src/main/java/ru/abrarov/javatests/topbuilder/NaiveFrequencyAnalyzer.java
@@ -1,5 +1,7 @@
 package ru.abrarov.javatests.topbuilder;
 
+import ru.abrarov.javatests.topbuilder.example.CollectionUtils;
+
 import java.util.*;
 
 /**
@@ -107,46 +109,7 @@ public class NaiveFrequencyAnalyzer implements FrequencyAnalyzer {
      * @return List of items of the given distribution having max frequency.
      */
     private List<Item> findTopFrequentItems(Collection<Item> distribution, int count) {
-        return partialSorted(distribution, count, DISTRIBUTION_ITEM_COMPARATOR);
-    }
-
-    /**
-     * Builds a list consisting of first items of the sorted original collection.
-     *
-     * @param collection Collection to sort. Isn't modified.
-     * @param count      Number of items to be returned in a built collection. If greater than the size of collection
-     *                   then the last will be used.
-     * @param comparator The comparator to determine the order of the collection.
-     * @return List consisting of first items of the sorted original collection.
-     */
-    private static <T> List<T> partialSorted(Collection<T> collection, int count, Comparator<? super T> comparator) {
-        assert count >= 0 : "Number of sorted items should be >= 0";
-
-        // Some simple optimizations
-        if (count < 1 || collection.isEmpty()) {
-            return Collections.emptyList();
-        }
-        if (count == 1) {
-            return Collections.singletonList(Collections.min(collection, comparator));
-        }
-        if (collection.size() <= count) {
-            final List<T> temp = new ArrayList<T>(collection);
-            Collections.sort(temp, comparator);
-            return temp;
-        }
-        // Remove conditional branch from the cycle by splitting the only cycle into 2 cycles.
-        final NavigableSet<T> set = new TreeSet<T>(comparator);
-        final Iterator<T> iterator = collection.iterator();
-        // The first cycle
-        for (int i = 0; i < count; ++i) {
-            set.add(iterator.next());
-        }
-        // The rest part of the cycle
-        while (iterator.hasNext()) {
-            set.add(iterator.next());
-            set.pollLast();
-        }
-        return new ArrayList<T>(set);
+        return CollectionUtils.partialSorted(distribution, count, DISTRIBUTION_ITEM_COMPARATOR);
     }
 
 }
---
+++ top-builder/src/main/java/ru/abrarov/javatests/topbuilder/example/CollectionUtils.java
@@ -0,0 +1,51 @@
+package ru.abrarov.javatests.topbuilder.example;
+
+import java.util.*;
+
+/**
+ * Utility algorithms for JCF.
+ */
+public class CollectionUtils {
+
+    private CollectionUtils() {
+    }
+
+    /**
+     * Builds a list consisting of first items of the sorted original collection.
+     *
+     * @param collection Collection to sort. Isn't modified.
+     * @param count      Number of items to be returned in a built collection. If greater than the size of collection
+     *                   then the last will be used.
+     * @param comparator The comparator to determine the order of the collection.
+     * @return List consisting of first items of the sorted original collection.
+     */
+    public static <T> List<T> partialSorted(Collection<T> collection, int count, Comparator<? super T> comparator) {
+        assert count >= 0 : "Number of sorted items should be >= 0";
+
+        // Some simple optimizations
+        if (count < 1 || collection.isEmpty()) {
+            return Collections.emptyList();
+        }
+        if (count == 1) {
+            return Collections.singletonList(Collections.min(collection, comparator));
+        }
+        if (collection.size() <= count) {
+            final List<T> temp = new ArrayList<T>(collection);
+            Collections.sort(temp, comparator);
+            return temp;
+        }
+        // Remove conditional branch from the cycle by splitting the only cycle into 2 cycles.
+        final NavigableSet<T> set = new TreeSet<T>(comparator);
+        final Iterator<T> iterator = collection.iterator();
+        // The first cycle
+        for (int i = 0; i < count; ++i) {
+            set.add(iterator.next());
+        }
+        // The rest part of the cycle
+        while (iterator.hasNext()) {
+            set.add(iterator.next());
+            set.pollLast();
+        }
+        return new ArrayList<T>(set);
+    }
+}

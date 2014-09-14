5b8527bd0b6b88d4939550c2c0303bef185821ff
Marat Abrarov <abrarov@gmail.com>
2014-09-15 00:10:49 +0400

    Jar test was added.

--- top-builder/integration-test/src/test/java/ru/abrarov/javatests/topbuilder/NaiveFrequencyAnalyzerIntegrationTest.java
+++ top-builder/integration-test/src/test/java/ru/abrarov/javatests/topbuilder/NaiveFrequencyAnalyzerIntegrationTest.java
@@ -7,6 +7,10 @@ import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URL;
+
+import static org.junit.Assert.assertTrue;
+
 public class NaiveFrequencyAnalyzerIntegrationTest {
 
     private static final Logger logger = LoggerFactory.getLogger(NaiveFrequencyAnalyzerIntegrationTest.class);
@@ -25,7 +29,9 @@ public class NaiveFrequencyAnalyzerIntegrationTest {
 
     @Test
     public void testClassPath() {
-        logger.info(FrequencyAnalyzer.class.getProtectionDomain().getCodeSource().getLocation().toString());
+        final URL frequencyAnalyzerLocation = FrequencyAnalyzer.class.getProtectionDomain().getCodeSource().getLocation();
+        assertTrue(frequencyAnalyzerLocation.getFile().endsWith(".jar"));
+        logger.info(frequencyAnalyzerLocation.toString());
     }
 
 
